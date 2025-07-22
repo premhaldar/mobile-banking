3be36d78c7f9ce5ab39749384c81073be51f2a38
Prem Haldar <62010534+premhaldar@users.noreply.github.com>
2025-07-23 02:48:58 +0600

    remove path home page

--- js/login.js
+++ js/login.js
@@ -13,7 +13,7 @@ document.getElementById('btn-login').addEventListener('click', function (event) 
         phoneInput.value = '';
         pinInput.value = '';
     } else {
-        window.location.href='/home.html'
+        window.location.href='home.html'
         alert('Successfully Logged In!');
     }
 });

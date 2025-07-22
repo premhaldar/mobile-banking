de4ba20fd664423b56927b742a28f87d5affa41f
Prem Haldar <62010534+premhaldar@users.noreply.github.com>
2025-07-23 02:36:51 +0600

    Initial commit with login and add money design

---
+++ js/login.js
@@ -0,0 +1,20 @@
+document.getElementById('btn-login').addEventListener('click', function (event) {
+    event.preventDefault(); // ফর্ম রিলোড আটকায়
+
+    const phoneInput = document.getElementById('phone-number');
+    const pinInput = document.getElementById('pin-number');
+
+    const phone = phoneInput.value.trim();
+    const pin = pinInput.value.trim();
+
+    if (phone !== '01521314614' || pin !== '1234') {
+        alert('Invalid Phone or Pin!');
+         // Clear input fields here
+        phoneInput.value = '';
+        pinInput.value = '';
+    } else {
+        window.location.href='/home.html'
+        alert('Successfully Logged In!');
+    }
+});
+

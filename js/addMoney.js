c91962c02c65e254881b65e6dd02563f8eb0f62c
Prem Haldar <62010534+premhaldar@users.noreply.github.com>
2025-07-23 23:28:40 +0600

    transaction added

--- js/addMoney.js
+++ js/addMoney.js
@@ -1,4 +1,3 @@
-
 document.getElementById("btn-add-money").addEventListener("click", function () {
   const totalBalance = document.getElementById("balance").innerText;
   const balance = parseFloat(totalBalance.replace(/,/g, ""));
@@ -7,6 +6,38 @@ document.getElementById("btn-add-money").addEventListener("click", function () {
   const inputPin = document.getElementById("input-pin").value;
   if (inputPin === "1234") {
     const totalAdditionBalance = balance + addMoney;
+    // history
+    const date = new Date();
+    const formattedDate = date.toLocaleString("en-US", {
+      month: "short",
+      day: "2-digit",
+      year: "numeric",
+      hour: "2-digit",
+      minute: "2-digit",
+      hour12: true,
+    });
+
+    const tranAddMony = document.getElementById("transation-add-money");
+    tranAddMony.innerHTML += `
+    <div
+            class="transaction-item p-4 rounded-lg flex items-center justify-between"
+          >
+            <div class="flex items-center">
+              <div
+                class="w-10 h-10 bg-green-500 rounded-full flex items-center justify-center mr-4"
+              >
+                <i class="fas fa-plus text-white"></i>
+              </div>
+              <div>
+                <p class="text-white font-semibold">Add Money</p>
+                <p class="text-white/70 text-sm">${formattedDate}</p>
+              </div>
+            </div>
+            <p id="tran-add-money" class="text-green-400 font-bold">+৳${addMoney}</p>
+          </div>
+
+    `;
+
     document.getElementById("balance").innerText =
       totalAdditionBalance.toLocaleString();
     closeModal("addMoneyModal");
--- js/cashOut.js
+++ js/cashOut.js
@@ -6,6 +6,38 @@ document.getElementById("btn-cash-out").addEventListener("click", function () {
   const inputPin = document.getElementById("input-pin-number").value;
   if (inputPin === "1234") {
     const totalSubtractionBalance = balance - CashOut;
+
+    const date = new Date();
+    const formattedDate = date.toLocaleString("en-US", {
+      month: "short",
+      day: "2-digit",
+      year: "numeric",
+      hour: "2-digit",
+      minute: "2-digit",
+      hour12: true,
+    });
+
+    const tranAddMony = document.getElementById("transation-add-money");
+    tranAddMony.innerHTML += `
+    <div
+            class="transaction-item p-4 rounded-lg flex items-center justify-between"
+          >
+            <div class="flex items-center">
+              <div
+                class="w-10 h-10 bg-red-500 rounded-full flex items-center justify-center mr-4"
+              >
+                <i class="fas fa-minus text-white"></i>
+              </div>
+              <div>
+                <p class="text-white font-semibold">Cash Out</p>
+                <p class="text-white/70 text-sm">${formattedDate}</p>
+              </div>
+            </div>
+            <p class="text-red-400 font-bold">-৳${CashOut}</p>
+          </div>
+
+    `;
+
     document.getElementById("balance").innerText =
       totalSubtractionBalance.toLocaleString();
     closeModal("cashOutModal");
